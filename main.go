ada7b660d9edb443f8e186bffebb3d2fef4bcdb8
chentaihan <chentaihan@qutoutiao.net>
2019-12-30 18:56:43 +0800

    merge

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"github.com/chentaihan/leetcode/sortTag"
 )
 
 func main() {
 	sortTag.TestMerge()
-=======
-	"github.com/chentaihan/leetcode/dynamicTag"
-)
-
-func main() {
-	dynamicTag.TestlengthOfLIS()
->>>>>>> 3a3a5ba069f5abd3ee3a18cfc3744190b158f1b8
 }
